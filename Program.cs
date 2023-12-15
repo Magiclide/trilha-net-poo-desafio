3c40b156e801f72256120683d8f1d106d74e91b0
Enzo Fernandes Monteiro <enzo.monteiro@hotmail.com>
2023-12-14 22:07:44 -0300

    Desafio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smarthphone Nokia: ");
+Smartphone Nokia = new Nokia("9977477077","J7","1330",8);
+Nokia.ReceberLigacao();
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Whatsapp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Smartphone Iphone = new Iphone("9974522545","A2","2550",8);
+Console.WriteLine("Smarthphone Iphone: ");
+Iphone.ReceberLigacao();
+Iphone.Ligar();
+Iphone.InstalarAplicativo("Telegram");
+
